2d0f9b065bca1d663a064c5b01d6c7a65b1d5f1b
V7ctor <joaovictor89437@gmail.com>
2022-03-07 13:13:54 -0300

    Resolução de alguns bugs

--- views-cache/payment-pagseguro.7171442309116ce03f28f5321dbfddbd.rtpl.php
+++ views-cache/payment-pagseguro.7171442309116ce03f28f5321dbfddbd.rtpl.php
@@ -6,7 +6,7 @@
         
         <?php $counter1=-1;  if( isset($products) && ( is_array($products) || $products instanceof Traversable ) && sizeof($products) ) foreach( $products as $key1 => $value1 ){ $counter1++; ?> 
         <input name="itemId<?php echo htmlspecialchars( $counter1+1, ENT_COMPAT, 'UTF-8', FALSE ); ?>" type="hidden" value="<?php echo htmlspecialchars( $value1["idproduct"], ENT_COMPAT, 'UTF-8', FALSE ); ?>">  
-        <input name="itemDescription<?php echo htmlspecialchars( $counter1+1, ENT_COMPAT, 'UTF-8', FALSE ); ?>" type="hidden" value="<?php echo htmlspecialchars( $value1["desproduct"], ENT_COMPAT, 'UTF-8', FALSE ); ?>">  
+        <input name="itemDescription<?php echo htmlspecialchars( $counter1+1, ENT_COMPAT, 'UTF-8', FALSE ); ?>" type="hidden" value="<?php echo htmlspecialchars( $value1["nameproduct"], ENT_COMPAT, 'UTF-8', FALSE ); ?>">  
         <input name="itemAmount<?php echo htmlspecialchars( $counter1+1, ENT_COMPAT, 'UTF-8', FALSE ); ?>" type="hidden" value="<?php echo htmlspecialchars( $value1["vltotal"], ENT_COMPAT, 'UTF-8', FALSE ); ?>">  
         <input name="itemQuantity<?php echo htmlspecialchars( $counter1+1, ENT_COMPAT, 'UTF-8', FALSE ); ?>" type="hidden" value="<?php echo htmlspecialchars( $value1["nrqtd"], ENT_COMPAT, 'UTF-8', FALSE ); ?>">  
         <input name="itemWeight<?php echo htmlspecialchars( $counter1+1, ENT_COMPAT, 'UTF-8', FALSE ); ?>" type="hidden" value="<?php echo htmlspecialchars( $value1["vlweight"]*1000, ENT_COMPAT, 'UTF-8', FALSE ); ?>">  
@@ -17,20 +17,20 @@
           
         <!-- Informações de frete (opcionais) -->  
         <input name="shippingType" type="hidden" value="1">
-        <input name="shippingAddressPostalCode" type="hidden" value="<?php echo htmlspecialchars( $order["deszipcode"], ENT_COMPAT, 'UTF-8', FALSE ); ?>">  
-        <input name="shippingAddressStreet" type="hidden" value="<?php echo utf8_encode($order["desaddress"]); ?>">  
-        <input name="shippingAddressNumber" type="hidden" value="<?php echo utf8_encode($order["desnumber"]); ?>">  
-        <input name="shippingAddressComplement" type="hidden" value="<?php echo utf8_encode($order["descomplement"]); ?>">  
-        <input name="shippingAddressDistrict" type="hidden" value="<?php echo utf8_encode($order["desdistrict"]); ?>">  
-        <input name="shippingAddressCity" type="hidden" value="<?php echo utf8_encode($order["descity"]); ?>">
-        <input name="shippingAddressState" type="hidden" value="<?php echo utf8_encode($order["desstate"]); ?>"> 
-        <input name="shippingAddressCountry" type="hidden" value="<?php echo utf8_encode($order["descountry"]); ?>">  
+        <input name="shippingAddressPostalCode" type="hidden" value="<?php echo htmlspecialchars( $order["zipcode"], ENT_COMPAT, 'UTF-8', FALSE ); ?>">  
+        <input name="shippingAddressStreet" type="hidden" value="<?php echo utf8_encode($order["address"]); ?>">  
+        <input name="shippingAddressNumber" type="hidden" value="<?php echo utf8_encode($order["number"]); ?>">  
+        <input name="shippingAddressComplement" type="hidden" value="<?php echo utf8_encode($order["complement"]); ?>">  
+        <input name="shippingAddressDistrict" type="hidden" value="<?php echo utf8_encode($order["district"]); ?>">  
+        <input name="shippingAddressCity" type="hidden" value="<?php echo utf8_encode($order["city"]); ?>">
+        <input name="shippingAddressState" type="hidden" value="<?php echo utf8_encode($order["state"]); ?>"> 
+        <input name="shippingAddressCountry" type="hidden" value="<?php echo utf8_encode($order["country"]); ?>">  
   
         <!-- Dados do comprador (opcionais) -->  
-        <input name="senderName" type="hidden" value="<?php echo utf8_encode($order["desperson"]); ?>">  
+        <input name="senderName" type="hidden" value="<?php echo utf8_encode($order["nameperson"]); ?>">  
         <input name="senderAreaCode" type="hidden" value="<?php echo htmlspecialchars( $phone["areaCode"], ENT_COMPAT, 'UTF-8', FALSE ); ?>">  
         <input name="senderPhone" type="hidden" value="<?php echo htmlspecialchars( $phone["number"], ENT_COMPAT, 'UTF-8', FALSE ); ?>">  
-        <input name="senderEmail" type="hidden" value="<?php echo htmlspecialchars( $order["desemail"], ENT_COMPAT, 'UTF-8', FALSE ); ?>">  
+        <input name="senderEmail" type="hidden" value="<?php echo htmlspecialchars( $order["emailuser"], ENT_COMPAT, 'UTF-8', FALSE ); ?>">  
           
 </form>  
 <script type="text/javascript">
